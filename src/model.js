c7088b2fff9ba160362b602ae38b2d9292ce13a1
YUNGSHENG <pupugggg@gmail.com>
2022-06-08 18:15:06 +0800

    feat:added function to assign data to  shard

--- src/model.js
+++ src/model.js
@@ -1,18 +1,30 @@
-const { sequelize } = require('./schema')
+const { SHARD_POOL } = require('./schema')
+
+const assignShard = (shorten) => {
+    const SHARD_AMOUNT = SHARD_POOL.length
+    let sum = 0
+    for (let i = 0; i < shorten.length; i++) {
+        sum = sum + shorten.charCodeAt()
+    }
+    return sum % SHARD_AMOUNT
+}
 
 const storeShortenURL = async (shorten, origin) => {
     let result
-    try{
-     result = await sequelize.URLModel.create({
-        shorten: shorten,
-        origin: origin,
-    })
-    }catch(error){
+    let sequelize = SHARD_POOL[assignShard(shorten)]
+    try {
+        result = await sequelize.URLModel.create({
+            shorten: shorten,
+            origin: origin,
+        })
+    } catch (error) {
+        console.log(error.stack)
         return false
     }
     return result.shorten
 }
 const getOriginFromDB = async (shorten) => {
+    let sequelize = SHARD_POOL[assignShard(shorten)]
     const target = await sequelize.URLModel.findOne({
         where: { shorten: shorten },
     })
--- src/schema.js
+++ src/schema.js
@@ -1,22 +1,31 @@
-
 const { Sequelize, DataTypes } = require('sequelize')
-console.log(process.env.DB_URL)
-const sequelize = new Sequelize(process.env.DB_URL)
-const defineSchema = async () => {
-    const URLModel = sequelize.define('URL', {
-        shorten: {
-            type: DataTypes.STRING(7),
-            allowNull: false,
-            unique: true,
-        },
-        origin: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    },{timestamps:false})
-    sequelize.URLModel = URLModel
-    await sequelize.sync({ force: true })
-    
+const DB_URLs = process.env.SHARD_POOL.split(',')
+const SHARD_POOL = []
+for (let i = 0; i < DB_URLs.length; i++) {
+    SHARD_POOL.push(new Sequelize(DB_URLs[i],{define:{charset:'utf8',collate:'utf8_bin'}}))
 }
 
-module.exports = { defineSchema, sequelize }
+const defineSchema = async () => {
+    for (let i = 0; i < SHARD_POOL.length; i++) {
+
+        const URLModel = SHARD_POOL[i].define(
+            'URL',
+            {
+                shorten: {
+                    type: DataTypes.STRING(7),
+                    allowNull: false,
+                    unique: true,
+                },
+                origin: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                },
+            },
+            { timestamps: false }
+        )
+        SHARD_POOL[i].URLModel = URLModel
+        await SHARD_POOL[i].sync({ force: true })
+    }
+}
+
+module.exports = { defineSchema, SHARD_POOL }
